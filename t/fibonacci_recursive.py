67df13725d19c087e9c6c6f0f1379ba8e97e6050
QuentinCraft <91551241+QuentinCraft@users.noreply.github.com>
2025-10-02 12:29:10 +0200

    fix: delete whole repository

--- t/fibonacci_recursive.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_recursive(n):
-    if n <= 1:
-        return n
-    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
-
-if __name__ == "__main__":
-    n = 10
-    result = fibonacci_recursive(n)
-    print(f"Fibonacci({n}) = {result}")
